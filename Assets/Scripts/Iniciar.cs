3776d1b0d9354f5d76cdd9a56521f6477d52085a
fenixblanco18 <montero.helena9@gmail.com>
2024-04-10 18:00:16 +0200

    Creando Script

---
+++ Assets/Scripts/Iniciar.cs
@@ -0,0 +1,19 @@
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Iniciar : MonoBehaviour
+{
+    public void IniciarJuego(){
+        
+        SceneManager.LoadScene(1);
+
+    }
+}
+
+
+
+
