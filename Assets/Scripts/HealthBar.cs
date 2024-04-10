04f535edd93e8351ec1dbf6852782e31b20832e0
fenixblanco18 <montero.helena9@gmail.com>
2024-04-10 20:35:05 +0200

    Barra salud terminada

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Image barImage;
+
+    public void UpdateHealthbar(float maxHealth; float health)
+    {
+        barImage.fillAmount = HealthBar / maxHealth;
+
+    }
+    
+}
